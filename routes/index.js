256809b6f5b7eb39b7dc352408ae1f398aec325b
Ali-Arani <94089011+Ali-Arani@users.noreply.github.com>
2022-10-04 15:13:32 -0400

    ex2-ver1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(
+    'index', 
+    { 
+      title:'Home', 
+      name: 'Alireza'
+    }
+  );
 });
 
 module.exports = router;
