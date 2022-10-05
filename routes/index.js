3a5eef8c96b2c250db0d3962cc50c7286018172a
Ali-Arani <94089011+Ali-Arani@users.noreply.github.com>
2022-10-05 18:01:25 -0400

    addlink2

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,32 @@ router.get('/', function(req, res, next) {
   );
 });
 
+router.get('/about', function(req, res, next) {
+  res.render(
+    'about', 
+    { 
+      title:'About', 
+      name: 'Alireza'
+    }
+  );
+});
+router.get('/project', function(req, res, next) {
+  res.render(
+    'project', 
+    { 
+      title:'project', 
+      name: 'Alireza'
+    }
+  );
+});
+router.get('/services', function(req, res, next) {
+  res.render(
+    'services', 
+    { 
+      title:'services', 
+      name: 'Alireza'
+    }
+  );
+});
+
 module.exports = router;
